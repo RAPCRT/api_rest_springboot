ff990a36d1f04bcf32872ac1b7d9ef13cd0707db
RAPCAT <robertopizarro199@gmail.com>
2025-08-31 00:11:39 -0400

    Primer guardado de destructura del proyecto

---
+++ src/test/java/cl/kibernumacademy/rest/techstore/TechstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.kibernumacademy.rest.techstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
